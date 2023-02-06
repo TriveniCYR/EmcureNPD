3e629154e618b73a51c20f5edb3dfb026a9af3ec
kuldip-wagh-neosoft <kuldip.wagh@neosoftmail.com>
2023-02-06 17:53:27 +0530

    Role Mange CheckBox and Commercial form UI changes

---
+++ NPD/EmcureNPD.Web/wwwroot/js/Custom/Role/RoleManage.js
@@ -0,0 +1,74 @@
+﻿
+$(document).ready(function () {
+    debugger;
+   // SetupRoleTable();
+});
+
+$('#chkView').change(function () {
+
+    if ($(this).is(':checked')) {
+        $("input[name*='RoleModulePermission.View']").prop('checked', true);
+    }
+    else {
+        $("input[name*='RoleModulePermission.View']").prop('checked', false);
+    }
+});
+
+$('#chkView').change(function () {
+
+    if ($(this).is(':checked')) {
+        $("input[name*='RoleModulePermission.View']").prop('checked', true);
+    }
+    else {
+        $("input[name*='RoleModulePermission.View']").prop('checked', false);
+    }
+});
+
+$('#chkView').change(function () {
+
+    if ($(this).is(':checked')) {
+        $("input[name*='RoleModulePermission.View']").prop('checked', true);
+    }
+    else {
+        $("input[name*='RoleModulePermission.View']").prop('checked', false);
+    }
+});
+
+$('#chkAdd').change(function () {
+
+    if ($(this).is(':checked')) {
+        $("input[name*='RoleModulePermission.Add']").prop('checked', true);
+    }
+    else {
+        $("input[name*='RoleModulePermission.Add']").prop('checked', false);
+    }
+});
+
+$('#chkEdit').change(function () {
+
+    if ($(this).is(':checked')) {
+        $("input[name*='RoleModulePermission.Edit']").prop('checked', true);
+    }
+    else {
+        $("input[name*='RoleModulePermission.Edit']").prop('checked', false);
+    }
+});
+$('#chkDelete').change(function () {
+
+    if ($(this).is(':checked')) {
+        $("input[name*='RoleModulePermission.Delete']").prop('checked', true);
+    }
+    else {
+        $("input[name*='RoleModulePermission.Delete']").prop('checked', false);
+    }
+});
+$('#chkApprove').change(function () {
+
+    if ($(this).is(':checked')) {
+        $("input[name*='RoleModulePermission.Approve']").prop('checked', true);
+    }
+    else {
+        $("input[name*='RoleModulePermission.Approve']").prop('checked', false);
+    }
+});
+
