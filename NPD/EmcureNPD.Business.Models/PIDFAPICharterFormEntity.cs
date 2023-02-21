791d7a5e18483f83794727f1460edaa3321f758a
Kuldip.Wagh <Kuldip.Wagh@PIDF-DEV>
2023-02-21 20:21:26 +0530

    API charter View Model changes

---
+++ NPD/EmcureNPD.Business.Models/PIDFAPICharterFormEntity.cs
@@ -0,0 +1,72 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmcureNPD.Business.Models
+{
+    
+    public class PIDFAPICharterFormEntity
+    {
+        public long PIDFAPICharterFormID { get; set; }        
+        public string Pidfid { get; set; }
+        public string SaveType { get; set; }
+        public string ProjectName { get; set; }
+        public string ManHourRates { get; set; }
+        public string APIGroupLeader { get; set; }
+        public int ProjectComplexityId { get; set; }        
+        public int LoggedInUserId { get; set; }
+        //public List<PIDF_IPD_PatentDetailsEntity> IPD_PatentDetailsList { get; set; }
+        [Required]
+        public string IsModelValid { get; set; }
+              
+    }
+    //-------------------Child Table--------------------------  
+    public class CapitalOtherExpenditure
+    {
+        public long PIDFAPICharterID { get; set; }
+        public string Name { get; set; }
+        public string Amount { get; set; }
+        public string Remark { get; set; }      
+    }
+    public class AnalyticalDepartment
+    {
+        public long PIDFAPICharterID { get; set; }
+        public string Name { get; set; }
+        public string Amount { get; set; }
+        public string Remark { get; set; }
+    }
+    public class HeadwiseBudget
+    {
+        public long PIDFAPICharterID { get; set; }
+        public string Name { get; set; }
+        public string Amount { get; set; }
+        public string Remark { get; set; }
+    }
+    public class ManhourEstimates
+    {
+        public long PIDFAPICharterID { get; set; }
+        public string Name { get; set; }
+        public string Amount { get; set; }
+        public string Remark { get; set; }
+    }
+    public class Charter_PRDDepartment
+    {
+        public long PIDFAPICharterID { get; set; }
+        public string Name { get; set; }
+        public string Amount { get; set; }
+        public string Remark { get; set; }
+    }
+    public class TimelineInMonths
+    {
+        public long PIDFAPICharterID { get; set; }
+        public string Name { get; set; }
+        public string Amount { get; set; }
+        public string Remark { get; set; }
+    } 
+}
