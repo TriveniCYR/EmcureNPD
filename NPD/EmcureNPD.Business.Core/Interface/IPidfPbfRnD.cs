29509c4c84cec31d6fbe6f2d091d5476f19ce181
Kamal <pandey.kp.kamal@gmail.com>
2023-03-16 21:23:26 +0530

    Rnd-inproress

---
+++ NPD/EmcureNPD.Business.Core/Interface/IPidfPbfRnD.cs
@@ -0,0 +1,21 @@
+﻿using EmcureNPD.Business.Models;
+using EmcureNPD.Data.DataAccess.Entity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static EmcureNPD.Utility.Enums.GeneralEnum;
+
+namespace EmcureNPD.Business.Core.Interface
+{
+	public interface IPidfPbfRnD
+	{
+		Task<List<PidfPbfRnDEntity>> GetAll();
+
+		Task<PidfPbfRnDEntity> GetById(int id);
+
+		Task<DBOperation>AddUpdate(PidfPbfRnDEntity EntitypidfRnd);
+		Task<dynamic> GetPidfPbfRnD(int PidfPbfRnDId = 0);
+	}
+}
