34860519d4008dfd32b59400dc26ae189b9a422f
Kamal <pandey.kp.kamal@gmail.com>
2023-04-13 19:54:35 +0530

    ENP-211-partially-done

--- NPD/EmcureNPD.Web/Controllers/ProjectController.cs
+++ NPD/EmcureNPD.Web/Controllers/ProjectController.cs
@@ -38,10 +38,56 @@ namespace EmcureNPD.Web.Controllers
             return View();
         }
         [HttpPost]
-        public IActionResult AddUpdateTask(ProjectTaskEntity addTask, string id = "", string act = "", string buid="")
+        public IActionResult AddUpdateGanttTask([FromBody] ProjectTaskEntity addTask, string id = "", string act = "", string buid="")
         {
            // addTask.ProjectTaskId = projecttaskid;
            
+            if (addTask.ProjectTaskId > 0)
+            {
+                addTask.Pidfid = long.Parse(UtilityHelper.Decreypt(id));
+                addTask.ModifyBy = Convert.ToInt32(HttpContext.Session.GetString(UserHelper.LoggedInUserId));
+                addTask.ModifyDate = DateTime.Now;
+                if (act == "Task")
+                    addTask.TaskLevel = 1;
+                else if (act == "SubTask")
+                    addTask.TaskLevel = 2;
+            }
+            else
+            {
+                if (act == "Task")
+                    addTask.TaskLevel = 1;
+                else if (act == "SubTask")
+                    addTask.TaskLevel = 2;
+                addTask.Pidfid = long.Parse(UtilityHelper.Decreypt(id));
+                addTask.CreatedBy = Convert.ToInt32(HttpContext.Session.GetString(UserHelper.LoggedInUserId));
+                addTask.CreatedDate = DateTime.Now;
+            }
+
+            HttpContext.Request.Cookies.TryGetValue(UserHelper.EmcureNPDToken, out string token);
+            APIRepository objapi = new(_configuration);
+            string formData = JsonConvert.SerializeObject(addTask);
+
+            HttpResponseMessage responseMessage = objapi.APICommunication(APIURLHelper.AddUpdateTask, HttpMethod.Post, token, new StringContent(formData)).Result;
+            string jsonResponse = responseMessage.Content.ReadAsStringAsync().Result;
+            var data = JsonConvert.DeserializeObject<APIResponseEntity<dynamic>>(jsonResponse);
+
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                TempData[UserHelper.SuccessMessage] = data._Message;
+                return Json("success-" + data._Message.ToString());
+            }
+            else
+            {
+                TempData[UserHelper.ErrorMessage] = data._Message;
+                ModelState.Clear();
+                return Json("Error-" + data._Message.ToString());
+            }
+        }
+        [HttpPost]
+        public IActionResult AddUpdateTask(ProjectTaskEntity addTask, string id = "", string act = "", string buid = "")
+        {
+            // addTask.ProjectTaskId = projecttaskid;
+
             if (addTask.ProjectTaskId > 0)
             {
                 addTask.ModifyBy = Convert.ToInt32(HttpContext.Session.GetString(UserHelper.LoggedInUserId));
@@ -69,16 +115,21 @@ namespace EmcureNPD.Web.Controllers
             if (responseMessage.IsSuccessStatusCode)
             {
                 TempData[UserHelper.SuccessMessage] = data._Message;
-                return RedirectToAction("ProjectManagement", "Project", new {pidfid = UtilityHelper.Encrypt(addTask.Pidfid.ToString()), bui= buid});
+                return RedirectToAction("ProjectManagement", "Project", new { pidfid = UtilityHelper.Encrypt(addTask.Pidfid.ToString()), bui = buid });
             }
             else
             {
                 TempData[UserHelper.ErrorMessage] = data._Message;
                 ModelState.Clear();
-                return RedirectToAction("ProjectManagement", "Project", new { pidfid = UtilityHelper.Encrypt(addTask.Pidfid.ToString()), bui = buid});
+                return RedirectToAction("ProjectManagement", "Project", new { pidfid = UtilityHelper.Encrypt(addTask.Pidfid.ToString()), bui = buid });
             }
         }
         public IActionResult Gantt(string pidfid)
+        {
+            ViewBag.id = pidfid;
+            return View();
+        }
+        public IActionResult GanttView(string pidfid)
         {
             ViewBag.id = pidfid;
             return View();
