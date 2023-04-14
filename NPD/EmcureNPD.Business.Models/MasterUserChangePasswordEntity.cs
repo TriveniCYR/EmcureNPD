d12af0b22cc1f3432b60c69a46602827401939ff
kuldip-wagh-neosoft <kuldip.wagh@neosoftmail.com>
2023-04-14 12:42:51 +0530

    added SMTP details in Appsetting

--- NPD/EmcureNPD.Business.Models/MasterUserChangePasswordEntity.cs
+++ NPD/EmcureNPD.Business.Models/MasterUserChangePasswordEntity.cs
@@ -15,7 +15,6 @@ namespace EmcureNPD.Business.Models
 
         [Display(Name = "Oldpassword", ResourceType = typeof(Master))]
         [Required(ErrorMessageResourceType = typeof(Validation), ErrorMessageResourceName = "Required")]
-        [StringLength(10, ErrorMessage = "Must be between 5 and 10 characters", MinimumLength = 5)]
         [DataType(DataType.Password)]
         public string Oldpassword { get; set; }
 
