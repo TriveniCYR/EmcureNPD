f8f44c219d56d63f6753b0e7a493d3df18462e57
Kamal <pandey.kp.kamal@gmail.com>
2023-04-14 17:15:17 +0530

    ENP-263-on-hold

--- NPD/EmcureNPD.Web/wwwroot/js/Custom/Gantt/updatedgantt.js
+++ NPD/EmcureNPD.Web/wwwroot/js/Custom/Gantt/updatedgantt.js
@@ -542,22 +542,25 @@
     gantt.config.auto_scheduling = false;
 
     var obj = gantt.json;
-    gantt.init("ganttContainer")//, new Date(2021, 1, 1,0,0,0), new Date(2022, 1, 1,0,0,0)); // initialize gantt
-    gantt.load({
-        "tasks": [
-            { "id": 1, "text": "Project #1", "start_date": "01-04-2020", "duration": 18 },
-            { "id": 2, "text": "Task #1", "start_date": "02-04-2020", "duration": 8, "parent": 1 },
-            { "id": 3, "text": "Task #2", "start_date": "11-04-2020", "duration": 8, "parent": 1 }
+    var data = {
+        tasks: [
+            { id: 1, text: "Project #1", start_date: "01-04-2023", duration: 1 },
+            { id: 2, text: "Task #1", start_date: "01-04-2023", duration: 1, parent: 1 },
+            { id: 3, text: "Task #2", start_date: "01-04-2023", duration: 1, parent: 1 }
         ],
-        "links": [
+        links: [
             { "id": 1, "source": 1, "target": 2, "type": "1" },
             { "id": 2, "source": 2, "target": 3, "type": "0" }
         ]
-    }, "json");
+    };
+    gantt.init("ganttContainer");
+    gantt.parse(data);
+    //gantt.init("ganttContainer")//, new Date(2021, 1, 1,0,0,0), new Date(2022, 1, 1,0,0,0)); // initialize gantt
+    gantt.load($('#hdnBaseURL').val() + "api/Project/GetTaskSubTask" + "/" + id, "json");
 
     //var dp = new gantt.dataProcessor("/api");
     var dp = new gantt.dataProcessor("/api")
-    dp.init(gantt);
+    dp.init(JSON.parse(JSON.stringify(gantt)));
     dp.setTransactionMode("REST-JSON");
 
 });
