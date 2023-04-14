9ed7a68bd869443e36f2679dcf2a1c3921be94d3
Prashant Yadav <prashant.yadav@neosoftmail.com>
2023-04-14 13:21:02 +0530

    commit notification non cache method

--- NPD/EmcureNPD.Business.Core/Interface/INotificationService.cs
+++ NPD/EmcureNPD.Business.Core/Interface/INotificationService.cs
@@ -16,7 +16,8 @@ namespace EmcureNPD.Business.Core.Interface {
         Task<DBOperation> CreateNotification(long pidfId, int statusid, string notificatioTitle, string notificationDescription, int loggedinUserId);
         Task<DBOperation> UpdateNotification(long notificationId,string notificationTitle, string notificationDescription, int loggedinUserId);
         Task<DataTableResponseModel> GetFilteredNotifications(string ColumnName, string SortDir, int start, int length, int RoleId);
-        Task<DBOperation> ClickedNotification();
+        Task<PendingNotification> ClickedNotification();
+        Task<PendingNotification> NotificationCountForUser();
 
     }
 }
