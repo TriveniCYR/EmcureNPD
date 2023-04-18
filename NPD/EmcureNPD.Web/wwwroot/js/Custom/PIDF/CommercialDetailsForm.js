795999aaa5d0d348e555dd0bb454760509b46c97
kuldip-wagh-neosoft <kuldip.wagh@neosoftmail.com>
2023-04-18 13:56:50 +0530

    Edit Year  details in Commercial

--- NPD/EmcureNPD.Web/wwwroot/js/Custom/PIDF/CommercialDetailsForm.js
+++ NPD/EmcureNPD.Web/wwwroot/js/Custom/PIDF/CommercialDetailsForm.js
@@ -5,7 +5,7 @@ var ColumnObjUpcase = ['CommercialPackagingTypeId', 'CommercialBatchSize', 'Pric
 /*var ColumnObjLowcase = ['packagingTypeId', 'commercialBatchSize', 'priceDiscounting', 'totalApireq', 'apireq', 'suimsvolume', 'marketGrowth', 'marketSize', 'priceErosion', 'finalSelectionId'];*/
 var SelectedBUValue = 0;
 var selectedStrength = 0;
-
+var EditIndex = -1;
 $(document).ready(function () {
     InitializeProductTypeDropdown();
     //SetCommercialDivReadonly();
@@ -194,7 +194,7 @@ function ResetYearFormValues() {
     });
 }
 
-function AddYearClick() {
+function AddYearClick() { //SaveYearClick
     //var year = $('#AddYearForm').serializeArray();
     var valBUStrength = ValidateBU_Strength();
     if (ValidateYearForm() && valBUStrength && ValidateMainForm()) {
@@ -208,15 +208,18 @@ function AddYearClick() {
             }
             entityYear[kv.name] = kv.value;
         });
-
-        //console.log(entityYear);
-        //entityYear['YearIndex'] = objYears.length + 1;
-        objYears.push(entityYear);
+       
+        if (EditIndex == -1)
+            objYears.push(entityYear);
+        else
+            objYears[EditIndex] = entityYear;
+        
         UpdateYearTable(ColumnObjUpcase);
         ClearValidationForYearForm();
         $("#AddYearForm").hide();
         IsShowCancel_Save_buttons(true);
         ResetYearFormValues();
+        EditIndex = -1;
     }
 }
 function AddCommercialRow(i) {
@@ -228,8 +231,19 @@ function AddColToRow(i, tdvalue) {
     var rowid = 'AddYearRow' + i;
     $('#' + rowid).append('<td>' + tdvalue + '</td>')
 }
-
+function editCommercialRow(i) {
+    EditIndex = i;
+    ShowYearForm(i + 1);
+   // $('#NspunitsHigh').focus();
+   // $('#btnCancelYearForm').focus();
+    var entityYear = objYears[i];
+    $.each($('#AddYearForm').serializeArray(), function (_, kv) {
+        $('#' + kv.name).val(entityYear[kv.name]);
+    });
+    $('#CommercialPackagingTypeId').focus();
+}
 function deleteCommercialRow(i) {
+    console.log(objYears);
     objYears.pop(i);
     UpdateYearTable(ColumnObjUpcase);
 }
@@ -305,7 +319,8 @@ function UpdateYearTable(columns) {
             }
         });
         AddtblRevenueRow(objYears[i], i, columns);
-        $('#AddYearRow' + i).append('<td> <spam><i class="fas fa-trash-alt" id="deleteIconAddyear_' + i + '" onclick="deleteCommercialRow(' + i + ')"></i></spam></td>')
+        $('#AddYearRow' + i).append('<td> <spam><i class="fa fa-fw fa-trash mr-1" id="deleteIconAddyear_' + i + '" onclick="deleteCommercialRow(' + i + ')"></i></spam>'+       
+        '<spam><i class="fas fa-edit" id="editIconAddyear_' + i + '" onclick="editCommercialRow(' + i + ')"></i></spam></td>')
 
         $('#AddYearRow' + i + ' td:eq(10) spam i').prop('id', 'deleteIconAPI_' + i + '');
         $('#AddYearRow' + i + ' td:eq(10) spam i').attr('onclick', 'deleteRowApiDetails(' + i + ')');
@@ -448,6 +463,9 @@ function SetCommercialDisableForOtherUserBU() {
 }
 
 // ----------Calulations Methods--------------
+
+
+
 //----------- Market Size-----------------------
 $('#MarketGrowth').focusout(function () {
     var result = 0;
@@ -499,12 +517,13 @@ function NSP(variable) {
     var result = 0;
     var Nspunits;
     var PriceErosion = $('#PriceErosion').val();
-    var objyearLen = objYears.length;
-    if (objyearLen == 0) {
+    
+    var currentIndex = (EditIndex == -1) ? objYears.length : EditIndex;
+    if (currentIndex == 0) {
         Nspunits = $('#Nspunits' + variable).val();
     }
-    if (objyearLen > 0) {
-        Nspunits = objYears[objyearLen - 1]['Nsp' + variable];
+    if (currentIndex > 0) {
+        Nspunits = objYears[currentIndex - 1]['Nsp' + variable];
     }
     result = Nspunits * (1 + (PriceErosion / 100))
     $('#Nsp' + variable).val(result.toFixed(3));
@@ -513,12 +532,20 @@ function ShowPopUpCommercial() {
     $("#CancelModelCommercial").find("button, submit, a").show();
     $('#CancelModel').modal('show');
 }
-function ShowYearForm() {
+function ShowYearForm(yearIndex) {
     if (ValidateBU_Strength()) {
         $("#AddYearForm").show();
         IsShowCancel_Save_buttons(false);
     }
-    var Count_Of_Next_year = objYears.length + 1;
+    var Count_Of_Next_year;
+    if (yearIndex == 0) {
+        EditIndex = -1;
+        Count_Of_Next_year = objYears.length + 1;
+    }
+    else {
+        Count_Of_Next_year = yearIndex;
+    }
+    
     var SaveButtonText = 'Save Year';
     $("#btnSaveYear").text(SaveButtonText + Count_Of_Next_year);
 }
