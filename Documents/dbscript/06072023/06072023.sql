1188fd6ce1130ef301072ed3b81e8e05e4ab961f
kuldip-wagh-neosoft <kuldip.wagh@neosoftmail.com>
2023-07-06 21:18:23 +0530

    IPD and PBF tab changes

---
+++ Documents/dbscript/06072023/06072023.sql
@@ -0,0 +1,13 @@
+alter table PIDF_IPD_PatentDetails 
+Add CountryId int
+,PatentStrategy int
+,PatentStrategyOther nvarchar(100)
+, BasicPatentExpiry date
+, OtherLmitingPatentDate1 date
+, OtherLmitingPatentDate2 date
+, EarliestLaunchDate date
+, AnyPatentstobeFiled bit
+, EarliestMarketEntry date
+, stimatedNumberofgenericsinthe nvarchar(100)
+, Lawfirmbeingused nvarchar(100)
+
