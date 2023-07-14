4b0f9a056af785fdac2775632cea6fd804c72625
kuldip-wagh-neosoft <kuldip.wagh@neosoftmail.com>
2023-07-14 11:02:55 +0530

    bug fixes

--- Documents/dbscript/13072023/13072023.sql
+++ Documents/dbscript/13072023/13072023.sql
@@ -65,3 +65,74 @@ END
   insert into Master_Patent_Strategy values('Litigation')
   insert into Master_Patent_Strategy values('Negotiation')
   insert into Master_Patent_Strategy values('Other')
+
+
+  go
+--exec GetWishList  @PageSize = 10, @CurrentPageNumber = 0 ,@typeId=1  
+alter Proc [dbo].[GetWishList]  
+(            
+@typeId int=0,            
+@CurrentPageNumber INT = 0,                        
+@PageSize INT = 25,                        
+@SortColumn VARCHAR(100) = 'CreatedOn',                        
+@SortDirection VARCHAR(5) = 'DESC',                        
+@SearchText VARCHAR(MAX) =''            
+)                    
+AS                      
+BEGIN                
+            
+SET NOCOUNT ON;   
+Set @SearchText = IsNull(@SearchText, '')    
+Select * from (            
+SELECT Count(WishListId) OVER() TotalCount, * , ROW_NUMBER() OVER             
+(      
+ORDER BY             
+CASE                      
+WHEN @SortDirection <> 'DESC' THEN 0                      
+WHEN @SortColumn = 'WishListId' THEN  WishListId                       
+END DESC                       
+,CASE                      
+WHEN @SortDirection <> 'ASC' THEN 0                      
+WHEN @SortColumn = 'WishListId' THEN WishListId            
+END ASC             
+, CASE                      
+WHEN @SortDirection <> 'DESC' THEN null                      
+WHEN @SortColumn = 'CreatedOn' THEN CreatedOn                  
+END DESC                       
+,CASE                      
+WHEN @SortDirection <> 'ASC' THEN null                      
+WHEN @SortColumn = 'CreatedOn' THEN CreatedOn                      
+END ASC  
+,  
+CASE                      
+WHEN @SortDirection <> 'DESC' THEN null                      
+WHEN @SortColumn = 'MoleculeName' THEN MoleculeName                  
+END DESC                       
+,CASE                      
+WHEN @SortDirection <> 'ASC' THEN null                      
+WHEN @SortColumn = 'WishListTyp' THEN WishListTyp                      
+END ASC  
+,  
+CASE                      
+WHEN @SortDirection <> 'ASC' THEN null                      
+WHEN @SortColumn = 'WishListTyp' THEN WishListTyp                      
+END ASC  
+)             
+RowNumber from (            
+SELECT  COUNT(u.WishListId) OVER() TotalRecord,WishListId,U.WishListTypeId,WT.WishListTyp,GeographyId,MBU.BusinessUnitName as Geography,MC.CountryName,u.CountryId,  
+MoleculeName,Strength,IsInhouseOrInLicensed,DateOfFiling,DateOfApproval,NameofVendor,  
+VendorEvaluationRemark,ReferenceDrugProduct,Remarks,U.CreatedOn,U.UpdatedOn,U.DeletedOn,u.IsActive,u.CreatedBy           
+FROM Tbl_WishList As U     
+LEFT join Master_Country as MC on MC.CountryID = U.CountryId            
+LEFT join Master_User as us on us.UserId = u.CreatedBy         
+LEFT Join Master_BusinessUnit As MBU On MBU.BusinessUnitId = U.GeographyId   
+INNER JOIN Master_WishListType WT on WT.WishListTypeId=U.WishListTypeId  
+)    
+AS B WHERE               
+(MoleculeName LIKE '%' + @SearchText + '%') or (WishListTyp LIKE '%' + @SearchText + '%') or (Strength LIKE '%' + @SearchText + '%')   
+or (NameofVendor LIKE '%' + @SearchText + '%') or (CountryName LIKE '%' + @SearchText + '%') or (Geography LIKE '%' + @SearchText + '%') ) As C            
+Where  (WishListTypeId=@typeId or @typeId=0) and   
+RowNumber BETWEEN (@CurrentPageNumber) + 1 AND (@CurrentPageNumber) + @PageSize            
+            
+SET NOCOUNT OFF;   
+end  
