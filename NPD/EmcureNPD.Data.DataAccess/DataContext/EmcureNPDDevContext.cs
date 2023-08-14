378daa698545e89b3ce9e213e376285298f663ad
Kamal <pandey.kp.kamal@gmail.com>
2023-08-14 19:05:58 +0530

    Email-Notification-Service

--- NPD/EmcureNPD.Data.DataAccess/DataContext/EmcureNPDDevContext.cs
+++ NPD/EmcureNPD.Data.DataAccess/DataContext/EmcureNPDDevContext.cs
@@ -714,15 +714,7 @@ namespace EmcureNPD.Data.DataAccess.DataContext
 
                 entity.Property(e => e.Pidfid).HasColumnName("PIDFId");
 
-                entity.HasOne(d => d.Pidf)
-                    .WithMany(p => p.MasterNotifications)
-                    .HasForeignKey(d => d.Pidfid)
-                    .HasConstraintName("FK_Master_Notification_PIDF");
-
-                entity.HasOne(d => d.Status)
-                    .WithMany(p => p.MasterNotifications)
-                    .HasForeignKey(d => d.StatusId)
-                    .HasConstraintName("FK_Master_Notification_Master_PIDFStatus");
+                entity.Property(e => e.SentDatetime).HasColumnType("datetime");
             });
 
             modelBuilder.Entity<MasterNotificationUser>(entity =>
