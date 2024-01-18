c8fb7e71a1a3a532312994bc1ef0d12fdf24b859
YallaReddy <yalla.reddy@datatusk.com>
2024-01-18 18:15:36 +0530

    dummy

--- NPD/EmcureNPD.Web/Controllers/MastersController.cs
+++ NPD/EmcureNPD.Web/Controllers/MastersController.cs
@@ -240,7 +240,7 @@ namespace EmcureNPD.Web.Controllers
             int rolId = _helper.GetLoggedInRoleId();
             RolePermissionModel objPermssion = UtilityHelper.GetCntrActionAccess((int)ModulePermissionEnum.MasterManagement, rolId);
             if (objPermssion == null || !(objPermssion.View || objPermssion.Add || objPermssion.Edit || objPermssion.Delete))
-            {
+            { 
                 return RedirectToAction("AccessRestriction", "Home");
             }
             ViewBag.Access = objPermssion;
